904e5d98c54880526acb162180fdfb90e5c3c5f2
Luke Carl Thompson <lukecarlthompson@gmail.com>
2023-02-19 15:31:31 +0800

    style base page and layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script>
   import Header from './Header.svelte';
-  import './styles.css';
+  import './styles.scss';
 </script>
 
 <div class="app">
@@ -9,10 +9,6 @@
   <main>
     <slot />
   </main>
-
-  <footer>
-    <p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-  </footer>
 </div>
 
 <style>
@@ -25,23 +21,6 @@
   main {
     position: relative;
     flex: 1;
-  }
-
-  footer {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 12px;
-  }
-
-  footer a {
-    font-weight: bold;
-  }
-
-  @media (min-width: 480px) {
-    footer {
-      padding: 12px 0;
-    }
+    margin: 20px;
   }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,6 @@
   <meta name="description" content="Pixi Spine viewer" />
 </svelte:head>
 
-<h1>Pixi Spine viewer</h1>
 <PixiCanvas />
 
 <style lang="scss">
